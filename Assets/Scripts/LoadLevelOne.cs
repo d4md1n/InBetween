6dcd5815955feed1fa05d0a4266a8d68115b1273
d4md1n <d4md1n@hotmail.com>
2016-11-23 20:38:51 +0200

    start script moved to Scripts folder and renamed to LoadLevelOne

---
+++ Assets/Scripts/LoadLevelOne.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LoadLevelOne : MonoBehaviour {
+
+        public void QuitApplication()
+        {
+            Application.Quit();
+        }
+        public void PlayLevelOne()
+        {
+            SceneManager.LoadScene("demo");
+        }
+    }
+}
